aefafcbe2f88bd97d873ee117a34972e238dd6b1
lnsx <zhang13652103006@163.com>
2017-01-09 10:18:47 +0800

    新建inter

---
+++ app/src/main/java/com/zhangysh/project/chat/inter/CallObject.java
@@ -0,0 +1,10 @@
+package com.zhangysh.project.chat.inter;
+
+/**
+ * Created by Administrator on 2016/9/9 0009.
+ */
+public interface CallObject {
+    public static final int NULL=0;
+    public static final int USERORDERSTATUSMANAGER=1;
+    public static final int MASTERORDERSTATUSMANAGER=2;
+}
