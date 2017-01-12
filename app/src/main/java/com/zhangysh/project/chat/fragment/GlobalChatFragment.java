a1d1e215e3eb632a96a64096968c8b955fd4acff
lnsx <zhang13652103006@163.com>
2017-01-12 16:46:05 +0800

    完善

---
+++ app/src/main/java/com/zhangysh/project/chat/fragment/GlobalChatFragment.java
@@ -0,0 +1,38 @@
+package com.zhangysh.project.chat.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.zhangysh.project.chat.fragment.base.BaseFragement;
+
+/**
+ * Created by Administrator on 2017/1/12 0012.
+ */
+public class GlobalChatFragment extends BaseFragement {
+    @Override
+    protected View createView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return null;
+    }
+
+    @Override
+    protected void init(View view) {
+
+    }
+
+    @Override
+    protected void lazyLoad() {
+
+    }
+
+    @Override
+    protected void saveBundle(Bundle outState) {
+
+    }
+
+    @Override
+    protected void getSavedBundle(Bundle savedInstanceState) {
+
+    }
+}
